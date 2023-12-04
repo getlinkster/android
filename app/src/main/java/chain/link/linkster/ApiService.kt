0eb6dad80a5713f5a8010a9d61c8cb1ee62b4c39
Steve Smith <stevysmith.ss@gmail.com>
2023-12-04 16:47:51 +0000

    Add API calls for profile credentials

--- app/src/main/java/chain/link/linkster/ApiService.kt
+++ app/src/main/java/chain/link/linkster/ApiService.kt
@@ -2,7 +2,11 @@ package chain.link.linkster
 
 
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -12,8 +16,60 @@ interface ApiService {
         @Query("sessionID") sessionID: String,
         @Query("linkID") linkID: String
     ): Call<QRCodeResponse>
+
+    @GET("/v1/schemas")
+    @Headers("accept: application/json")
+    fun getSchemas(): Call<List<SchemaResponse>>
+
+    @POST("/v1/credentials")
+    @Headers("accept: application/json")
+    fun creatUserCredential(
+        @Body request: CreateUserCredentialRequest
+    ): Call<CredentialResponse>
+
+    @GET("/v1/credentials/{credentialId}/qrcode")
+    @Headers("accept: application/json")
+    fun getQRCodeForCredential(
+        @Path("credentialId") credentialId: String
+    ): Call<QRCodeResponse>
+
 }
 
 data class QRCodeResponse(
     val qrCodeLink: String
+)
+
+data class CreateUserCredentialRequest(
+    val credentialSchema: String,
+    val type: String,
+    val credentialSubject: CredentialUserSubject,
+    val expiration: String,
+    val signatureProof: Boolean,
+    val mtProof: Boolean
+)
+
+data class CredentialUserSubject(
+    val name: String,
+    val wallet: String,
+    val profession: String
+)
+
+data class CredentialResponse(
+    val id: String
+)
+
+data class SchemaResponse(
+    val bigInt: String,
+    val createdAt: String,
+    val description: String?,
+    val hash: String,
+    val id: String,
+    val title: String?,
+    val type: String,
+    val url: String,
+    val version: String
+)
+data class ApiError(
+    val message: String?,
+    val code: Int?
 )
--- app/src/main/java/chain/link/linkster/ui/profile/ProfileFragment.kt
+++ app/src/main/java/chain/link/linkster/ui/profile/ProfileFragment.kt
@@ -8,14 +8,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import chain.link.linkster.ClientManager
+import chain.link.linkster.CreateUserCredentialRequest
+import chain.link.linkster.CredentialResponse
+import chain.link.linkster.CredentialUserSubject
 import chain.link.linkster.MainActivity
 import chain.link.linkster.QRCodeResponse
 import chain.link.linkster.R
 import chain.link.linkster.RetrofitClient
+import chain.link.linkster.SchemaResponse
 import chain.link.linkster.databinding.FragmentProfileBinding
 import chain.link.linkster.ui.dialog.QRCodeDialogFragment
 import chain.link.linkster.ui.onboarding.DIDCreationActivity
@@ -31,6 +36,8 @@ class ProfileFragment : Fragment() {
     private val viewModel: ProfileViewModel by viewModels()
     private var _binding: FragmentProfileBinding? = null
     private lateinit var accountManager: AccountManager
+    private val schemaList = mutableListOf<SchemaResponse>()
+    private var profileCredentialId: String = ""
 
 
     // This property is only valid between onCreateView and
@@ -50,12 +57,14 @@ class ProfileFragment : Fragment() {
         accountManager = AccountManager.get(requireContext())
 
         binding.showMyCode.setOnClickListener {
-            createAuthLinkQRCode("e5d3d537-edb1-4302-ab36-b72f9c0c154f", "e5d3d537-edb1-4302-ab36-b72f9c0c154f")
+            getQRCodeForCredential(profileCredentialId)
         }
 
         binding.buttonLogout.setOnClickListener {
             logoutUser()
         }
+
+        binding.createCredential.setOnClickListener { createUserCredential() }
 
         viewModel.identities.observe(viewLifecycleOwner) { identities ->
             val dids = identities.map { it.did } // Extract DIDs from identities
@@ -73,6 +82,7 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.getIdentities(requireContext())
+        fetchSchemas()
     }
 
     override fun onDestroyView() {
@@ -80,11 +90,127 @@ class ProfileFragment : Fragment() {
         _binding = null
     }
 
+    private fun fetchSchemas() {
+        binding.progress.visibility = View.VISIBLE
+        RetrofitClient.instance.getSchemas().enqueue(object : Callback<List<SchemaResponse>> {
+            override fun onResponse(call: Call<List<SchemaResponse>>, response: Response<List<SchemaResponse>>) {
+                if (response.isSuccessful) {
+                    val schemas = response.body()
+                    println("Schemas: $schemas")
+                    schemas?.forEach { schema ->
+                        val schemaResponse = SchemaResponse(type = schema.type, id = schema.id, url = schema.url, title = schema.title, description = schema.description, version = schema.version, createdAt = schema.createdAt, hash = schema.hash, bigInt = schema.bigInt)
+                        schemaList.add(schemaResponse)
+                        binding.progress.visibility = View.GONE
+                    }
+                } else {
+                    binding.progress.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Error loading schemas", Toast.LENGTH_SHORT).show()
+                    // Handle failure, possibly logging the error or showing a message
+                }
+            }
+
+            override fun onFailure(call: Call<List<SchemaResponse>>, t: Throwable) {
+                binding.progress.visibility = View.GONE
+                Toast.makeText(requireContext(), "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                println("Error: ${t.message}")
+                // Handle the error, such as network issues
+            }
+        })
+    }
+
+
     private fun showQRCodeDialog(qrCodeBitmap: Bitmap) {
         val dialog = QRCodeDialogFragment().apply {
             setQRCodeImage(qrCodeBitmap)
         }
         dialog.show(parentFragmentManager, "QRCodeDialog")
+    }
+
+    private fun createUserCredential() {
+        val linksterUserSchema = schemaList?.find { it.type == "LINKSTERUSER" }
+        if (linksterUserSchema != null) {
+            val credentialSchema = linksterUserSchema.url
+            val type = linksterUserSchema.type
+            val expiration = "2023-12-06T16:09:43.731Z"
+            val signatureProof = true
+            val mtProof = true
+
+            val credentialSubject = CredentialUserSubject(
+                name = "Steve Smith",
+                wallet = "0x2Ee9B2771602596A52a094ed74a71B02D499449A",
+                profession = "Software Engineer"
+            )
+
+            val request = CreateUserCredentialRequest(
+                credentialSchema = credentialSchema,
+                type = type,
+                credentialSubject = credentialSubject,
+                expiration = expiration,
+                signatureProof = signatureProof,
+                mtProof = mtProof
+            )
+
+            RetrofitClient.instance.creatUserCredential(request)
+                .enqueue(object : Callback<CredentialResponse> {
+                    override fun onResponse(
+                        call: Call<CredentialResponse>,
+                        response: Response<CredentialResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            val credentialId = response.body()?.id
+                            if (credentialId != null) {
+                                // Credential created successfully, you can handle the response here
+                                println("Credential created with ID: $credentialId")
+                                profileCredentialId = credentialId
+                            } else {
+                                // Handle the case where the response body is null
+                            }
+                        } else {
+                            // Handle the API request failure, possibly logging the error or showing a message
+                        }
+                    }
+
+                    override fun onFailure(call: Call<CredentialResponse>, t: Throwable) {
+                        println("Error: ${t.message}")
+                        // Handle the error, such as network issues
+                    }
+                })
+        } else {
+            Toast.makeText(requireContext(), "Schema not found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getQRCodeForCredential(credentialId: String) {
+        if (credentialId.isNotBlank()) {
+            RetrofitClient.instance.getQRCodeForCredential(credentialId)
+                .enqueue(object : Callback<QRCodeResponse> {
+                    override fun onResponse(
+                        call: Call<QRCodeResponse>,
+                        response: Response<QRCodeResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            val qrCodeUrl = response.body()?.qrCodeLink
+                            if (!qrCodeUrl.isNullOrEmpty()) {
+                                // Handle the QR code URL, you can display it or perform further actions
+                                // For example, you can generate a QR code image and display it
+                                val qrCodeBitmap = generateQRCodeBitmap(qrCodeUrl)
+                                showQRCodeDialog(qrCodeBitmap)
+                            } else {
+                                // Handle the case where the QR code URL is empty
+                            }
+                        } else {
+                            // Handle API error or non-successful response
+                        }
+                    }
+
+                    override fun onFailure(call: Call<QRCodeResponse>, t: Throwable) {
+                        // Handle network or request failure
+                        println("Error: ${t.message}")
+                    }
+                })
+        } else {
+            Toast.makeText(requireContext(), "Credential ID is empty", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun createAuthLinkQRCode(sessionID: String, linkID: String) {
